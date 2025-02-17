4c4095c01dd381a37a9f6da710841d0a3adee11b
code-hyun <122762287+code-hyun@users.noreply.github.com>
2025-02-18 00:27:22 +0900

    socket setting 8

---
+++ chattingServer/config/protocol.js
@@ -0,0 +1,8 @@
+class SocketHeader {
+    constructor(chattingroom, receiver) {
+        this.chattingroom = chattingroom;
+        this.receiver = receiver;
+    }
+}
+
+module.exports = { SocketHeader };
